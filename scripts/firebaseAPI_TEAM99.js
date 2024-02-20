707b09f107f8fabff3e169fe5451c098ca0435e9
MARIKO HOCKERTZ <mariko.hockertz@gmail.com>
2024-02-20 10:25:27 -0800

    demo07 complete

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyA5z4866es6NFHgwJwaipTcQtOq4dRRBUM",
+    authDomain: "comp1800-202410-demo-400b2.firebaseapp.com",
+    projectId: "comp1800-202410-demo-400b2",
+    storageBucket: "comp1800-202410-demo-400b2.appspot.com",
+    messagingSenderId: "544754777404",
+    appId: "1:544754777404:web:f9fc8a3839fcef7a59f239"
 };
 
 //--------------------------------------------
---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+function getNameFromAuth() {
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      console.log("user is logged in")
+      console.log(user.displayName)
+
+      document.getElementById("name-goes-here").innerHTML = user.displayName
+
+    } 
+    else {
+      console.log("user is NOT logged in")
+    }
+  })
+}
+getNameFromAuth()
